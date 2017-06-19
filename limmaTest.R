09f5be2e0b688c6b8f588d1c8825cc94e82886af
Mary Hoekstra <mary.hoekstra@queensu.ca>
2017-06-19 14:20:53 -0400

    updated limma function to account for correlation between same-patient samples

--- limmaTest.R
+++ limmaTest.R
@@ -1,10 +1,20 @@
 # perform a limma (Empirical Bayes) moderated t-test
 library(limma)
+library(statmod)
 
-runLimma <- function(exprsMatrix,designMatrix,contrastsString) {
+# given an expression set and contrasts, performs differential expression analysis on expression data
+# contrastsDesign is a design matrix with the two groups we are interested in comparing
+# contrastsString specifies how we wish to compare the two groups ("early-late","late-early",etc)
+runLimma <- function(exprsSet,designMatrix,contrastsString) {
+  
+  # account for correlations between samples of the same patient
+  patientIDs <- exprsSet$patient_id
+  corfit <- duplicateCorrelation(exprsSet,designMatrix,block=patientIDs)
+  # consensus represents the correlation between measurements made on the same patient 
+  print(corfit$consensus)
   
   # fit a linear model using the design
-  fit <- lmFit(exprsMatrix, designMatrix)
+  fit <- lmFit(exprsSet, designMatrix, block=patientIDs,correlation=corfit$consensus)
   
   # create a contrast matrix 
   # a positive logFC for a gene means that it's upregulated in the early group
@@ -18,4 +28,5 @@ runLimma <- function(exprsMatrix,designMatrix,contrastsString) {
 
   return(diffs)
 }
+
 
