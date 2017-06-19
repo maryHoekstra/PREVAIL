4102b903f967ff6f1f16ce193b1e932376cb9a95
Mary Hoekstra <mary.hoekstra@queensu.ca>
2017-06-19 14:21:36 -0400

    updated design matrix to have four levels (A.early, A.late, B.early, B.late)

--- earlyVsLate.R
+++ earlyVsLate.R
@@ -1,44 +1,42 @@
 # look at differential expression between early vs. late samples in group A and group B
 
-# subsets an expression set based on group code and samples from specific days
-getESet <- function(filteredESet,groupCode,earlyDays,lateDays) {
-  
-  # create expression set with desired group code
-  oneGroup.eset <- filtered.eset[ , filtered.eset$code==groupCode]
+# subsets an expression set based on samples from specific days
+getESet <- function(filteredESet,earlyDays,lateDays) {
   
   # create expression set with only early and late samples 
   earlyLateDays = c(earlyDays,lateDays)
-  earlyLateIndices <- logical(ncol(oneGroup.eset))
+  earlyLateIndices <- logical(ncol(filteredESet))
   for (i in 1:length(earlyLateDays)) {
-    earlyLateIndices <- earlyLateIndices | oneGroup.eset$Day==earlyLateDays[i]
+    earlyLateIndices <- earlyLateIndices | filteredESet$Day==earlyLateDays[i]
   }
-  
-  earlyLate.eset <- oneGroup.eset[,earlyLateIndices]
+  earlyLate.eset <- filteredESet[,earlyLateIndices]
   return (earlyLate.eset)
 }
 
 # creates design matrix using smaller expression set
 createDesign <- function(earlyLateESet) {  
+  earlyLateVec <- character(ncol(earlyLateESet))
   earlyIndices <- logical(ncol(earlyLateESet))
   for (i in 1:length(earlyDays)) {
     earlyIndices <- earlyIndices | earlyLateESet$Day==earlyDays[i]
   }
-  
+  earlyLateVec[earlyIndices]="early"
   lateIndices <- logical(ncol(earlyLateESet))
   for (i in 1:length(lateDays)) {
     lateIndices <- lateIndices | earlyLateESet$Day==lateDays[i]
   }
-  
-  # create design matrix with the two groups we are interested in 
-  design <- cbind(as.numeric(earlyIndices),as.numeric(lateIndices))
-  colnames(design) <- c('early', 'late')
-  return (design)
+  earlyLateVec[lateIndices]="late"
+  # create design matrix with the four groups we are interested in (A.early, A.late, etc) 
+  designVec <- factor(paste(earlyLateESet$code,earlyLateVec,sep="."))
+  designMatrix <- model.matrix(~0+designVec)
+  colnames(designMatrix) <- levels(designVec)
+  return (designMatrix)
 }
  
-# gets a table of top differentially expressed probes from limma analysis
-getDiffs <- function(earlyLateESet,design) {
-  EvsL <- "early-late"
-  diffs <- runLimma(exprs(earlyLateESet),design,EvsL)
+# gets a table of top differentially expressed probes for a given group from limma analysis
+getDiffs <- function(earlyLateESet,groupCode,design) {
+  contrastsString <- paste(groupCode,".early-",groupCode,".late",sep="")
+  diffs <- runLimma(earlyLateESet,design,contrastsString)
   # get entrez gene IDs for the differentially expressed probes
   diffs$ID <- as.factor(rownames(diffs))
   diffsWithAnno <- merge(diffs, dplyr::select(annoData, ID, Entrez.Gene))
@@ -61,19 +59,18 @@ getGeneList <- function(diffsWithAnno) {
 
 
 earlyDays <- c("day 1")
-lateDays <- c("day 7")
+lateDays <- c("day 14","day 21","day 28")
 
-earlyVsLateESet_A <- getESet(filtered.eset,'A',earlyDays,lateDays)
-designA <- createDesign(earlyVsLateESet_A)
-diffsA <- getDiffs(earlyVsLateESet_A,designA)
+earlyVsLateESet <- getESet(filtered.eset,earlyDays,lateDays)
+design <- createDesign(earlyVsLateESet)
+
+diffsA <- getDiffs(earlyVsLateESet,groupCode="A",design)
 geneListA <- getGeneList(diffsA)
-write(geneListA,file="/Users/maryhoekstra/Desktop/A_day1vs3.txt")
+write(geneListA,file="/Users/maryhoekstra/Desktop/A_new.txt")
 
-earlyVsLateESet_B <- getESet(filtered.eset,'B',earlyDays,lateDays)
-designB <- createDesign(earlyVsLateESet_B)
-diffsB <- getDiffs(earlyVsLateESet_B,designB)
+diffsB <- getDiffs(earlyVsLateESet,groupCode="B",design)
 geneListB <- getGeneList(diffsB)
-write(geneListB,file="/Users/maryhoekstra/Desktop/B.txt")
+write(geneListB,file="/Users/maryhoekstra/Desktop/B_new.txt")
 
 commonGenes <- intersect(geneListA,geneListB)
 write(commonGenes,file="/Users/maryhoekstra/Desktop/common.txt")
