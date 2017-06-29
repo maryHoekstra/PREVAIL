3bb1b275a8395ea28a9308e9a8b0d0749fe69337
Mary Hoekstra <mary.hoekstra@queensu.ca>
2017-06-29 09:52:35 -0400

    separate script for writing a gene list to a text file

---
+++ writeResults.R
@@ -0,0 +1,17 @@
+allGenes <- colnames(datExprs)
+
+getModuleGenes <- function(geneList,geneColours,moduleColour) {
+  moduleGenes <- (geneColours==moduleColour)
+  moduleIDs <- geneList[moduleGenes]
+  return (moduleIDs)
+}
+
+writeFile <- function(IDs,fileName) {
+  write(IDs,file=paste("/Users/maryhoekstra/Desktop/",fileName,".txt",sep=""))  
+}
+
+nonpreservedModules <- c("lightgreen","purple")
+for (i in 1:length(nonpreservedModules)) {
+  moduleIDs <- getModuleGenes(geneList=allGenes,geneColours = refColours,moduleColour = nonpreservedModules[i])
+  writeFile(moduleIDs,paste(nonpreservedModules[i],"_AvsB_late",sep = ""))
+}
